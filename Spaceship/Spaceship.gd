86571f11369b2a60eae34aa17adb015df0bea013
Elliot Croteau <elliot.cr@gmail.com>
2020-02-02 10:11:26 -0500

    hey :3

--- Spaceship/Spaceship.gd
+++ Spaceship/Spaceship.gd
@@ -10,24 +10,32 @@ export var ship: int
 func _on_Area2D_body_entered(body):
 	if body.filename == "res://Characters/Dino.tscn" && body.name == "Dino" + str(ship):
 		if body.item_picked_up != null:
-			print(body.item_picked_up)
 			match body.item_picked_up:
 				"res://Spaceship/asset/Canopy_Icon.png":
-					canopy = true
-					$Sprite/Canopy_Fixed.visible = true
-					body.item_picked_up = null
+					if canopy == false:
+						canopy = true
+						$Sprite/Canopy_Fixed.visible = true
+						body.item_picked_up = null
 					
 				"res://Spaceship/asset/Engine_Icon.png":
-					engine = true
-					$Sprite/Engine_Fixed.visible = true
-					body.item_picked_up = null
+					if engine == false:	
+						engine = true
+						$Sprite/Engine_Fixed.visible = true
+						body.item_picked_up = null
 
 				"res://Spaceship/asset/Haul_Icon.png":
-					haul = true
-					$Sprite/Haul_Fixed.visible = true
-					body.item_picked_up = null
+					if haul == false:	
+						haul = true
+						$Sprite/Haul_Fixed.visible = true
+						body.item_picked_up = null
 					
 				"res://Spaceship/asset/SS_Wing.png":
-					wing = true
-					$Sprite/SS_Wing.visible = true
-					body.item_picked_up = null
+					if wing == false:	
+						wing = true
+						$Sprite/SS_Wing.visible = true
+						body.item_picked_up = null
+	if (haul && wing && engine && canopy) == true:
+		print("Winner")
+		$Sprite.visible = false;
+		$SS_FIXED.visible = true;
+		body.position = position
