8e65be4deba92cbbed792ce3fc12b3c0a1ceb7d8
Olivier Demers <olidem123@gmail.com>
2020-02-01 21:24:10 -0500

    Added ground+ fixed meteor

--- Meteor/Meteor.gd
+++ Meteor/Meteor.gd
@@ -1,6 +1,7 @@
 extends RigidBody2D
 
 var item = preload("res://Spaceship/Collectible/Item.tscn")
+var dead = false
 
 func _ready():
 	randomize()
@@ -8,8 +9,13 @@ func _ready():
 
 func _process(delta):
 	var bodies = get_colliding_bodies()
-	if bodies.size() > 0:
+	if bodies.size() > 0 && !dead:
 		var object = item.instance()
 		object.position = get_global_position()
 		get_tree().get_root().add_child(object)
-		queue_free()
+		$Sprite.hide()
+		$Particles2D/Timer.start()
+		dead = true
+
+func _on_Timer_timeout():
+	queue_free()
