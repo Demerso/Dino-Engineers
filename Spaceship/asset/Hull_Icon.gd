81f2676e834fa77de8ff0335201ccbf216d97d8d
Sebastien Chanel <Sebastieninto@gmail.com>
2020-02-01 20:05:42 -0500

    Objects Update

---
+++ Spaceship/asset/Hull_Icon.gd
@@ -0,0 +1,19 @@
+extends Area2D
+
+var props 
+var for_tra = Vector2()
+var mov_speed = 0.3
+
+func _process(delta):
+	props = get_node("Hull_Icon")
+	props = translate(for_tra)
+
+func _on_Timer_timeout():
+	if for_tra == Vector2(0,-mov_speed):
+		for_tra = Vector2(0, mov_speed)
+	else:
+		for_tra = Vector2(0, -mov_speed)
+
+
+func _on_Hull_Icon_body_entered(body):
+	print("Body Entered - Hull Icon")
