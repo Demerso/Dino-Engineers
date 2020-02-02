985b7133b50afe8fb656305658913649cf13e073
Olivier Demers <olidem123@gmail.com>
2020-02-02 10:19:35 -0500

    Added repair sound

--- Spaceship/Spaceship.gd
+++ Spaceship/Spaceship.gd
@@ -16,24 +16,29 @@ func _on_Area2D_body_entered(body):
 						canopy = true
 						$Sprite/Canopy_Fixed.visible = true
 						body.item_picked_up = null
+						$RepairSound.play()
 					
 				"res://Spaceship/asset/Engine_Icon.png":
 					if engine == false:	
 						engine = true
 						$Sprite/Engine_Fixed.visible = true
 						body.item_picked_up = null
+						$RepairSound.play()
 
 				"res://Spaceship/asset/Haul_Icon.png":
 					if haul == false:	
 						haul = true
 						$Sprite/Haul_Fixed.visible = true
 						body.item_picked_up = null
+						$RepairSound.play()
 					
 				"res://Spaceship/asset/SS_Wing.png":
 					if wing == false:	
 						wing = true
 						$Sprite/SS_Wing.visible = true
 						body.item_picked_up = null
+						$RepairSound.play()
+						
 	if (haul && wing && engine && canopy) == true:
 		print("Winner")
 		$Sprite.visible = false;
